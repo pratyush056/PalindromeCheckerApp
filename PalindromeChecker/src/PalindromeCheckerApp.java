eb4c5bff9cdefef456c5eff5dee874c2bd504a50
pratyush056 <pratyushpriyansh5908@gmail.com>
2026-03-05 22:55:53 +0530

    UC11: Object-Oriented Palindrome Service

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -8,31 +8,34 @@ public class PalindromeCheckerApp {
             System.out.print("Input text: ");
             String input = scanner.nextLine();
 
-            boolean result = isPalindrome(input);
+            PalindromeChecker checker = new PalindromeChecker();
+            boolean result = checker.checkPalindrome(input);
 
             System.out.println("Is it a Palindrome? : " + result);
 
             scanner.close();
         }
-
-        public static boolean isPalindrome(String input) {
-            if (input == null || input.isEmpty()) return false;
-
-            String clean = input.replaceAll("\\s+", "").toLowerCase();
+}
 
 
-            return checkPalindrome(clean, 0, clean.length() - 1);
+        class PalindromeChecker {
 
-            
+            public boolean checkPalindrome(String input) {
+                if (input == null || input.isEmpty()) return false;
+
+                String clean = input.replaceAll("\\s+", "").toLowerCase();
+
+                return check(clean, 0, clean.length() - 1);
+            }
+
+            private boolean check(String str, int start, int end) {
+                if (start >= end)
+                    return true;
+
+                if (str.charAt(start) != str.charAt(end))
+                    return false;
+
+                return check(str, start + 1, end - 1);
+            }
         }
-
-        public static boolean checkPalindrome(String str, int start, int end) {
-            if (start >= end)
-                return true;
-
-            if (str.charAt(start) != str.charAt(end))
-                return false;
-
-            return checkPalindrome(str, start + 1, end - 1);
-        }
-    }
+    
